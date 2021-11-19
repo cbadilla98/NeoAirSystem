4604556e122af6d48993e2ae84c0d26aa3858b75
fabianEspn404 <86490258+fabianEspn404@users.noreply.github.com>
2021-11-18 22:02:31 -0600

    Basic Template Added

--- client/platform/src/app/app.component.ts
+++ client/platform/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss','../assets/css/layout.css','../assets/css/reset.css','../assets/css/style.css']
 })
 export class AppComponent {
   title = 'Neo Air System'; 
