b885dbe57cf2e1a665afac8d25f06b0e9923a203
fabianEspn404 <86490258+fabianEspn404@users.noreply.github.com>
2021-11-18 19:56:35 -0600

    Components folder

--- client/platform/src/app/app.component.ts
+++ client/platform/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'platform';
+  title = 'Neo Air System'; 
 }
--- client/platform/src/app/app.module.ts
+++ client/platform/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RutasComponent } from './components/rutas/rutas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RutasComponent
   ],
   imports: [
     BrowserModule,
