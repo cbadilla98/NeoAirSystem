8d5524acc8b98609246b7fc4878a1befe76b4451
Johan-CHOY <johanchaves453@gmail.com>
2021-11-24 21:53:48 -0600

    khá

--- client/platform/src/app/modulos/admin/tipo-avion-form/tipo-avion-form.component.ts
+++ client/platform/src/app/modulos/admin/tipo-avion-form/tipo-avion-form.component.ts
@@ -2,58 +2,83 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TipoAvionesService } from 'src/app/services/admin-tipoAviones/tipoAviones.service';
 
-interface ITipoAviones {
-  marca: '';
-  modelo: '';
-  annio: 0;
-  cantPasajeros: 0; 
-  cantFilas: 0;
-  cantAsientosFila: 0; 
-}
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-tipo-avion-form',
   templateUrl: './tipo-avion-form.component.html',
   styleUrls: ['./tipo-avion-form.component.scss']
 })
-export class TipoAvionFormComponent implements OnInit {
+export class tipoAvionFormComponent implements OnInit {
+  postForm = new FormGroup({
 
-  constructor(private tipoAvionesService: TipoAvionesService) { }
-
-  tipoAviones: ITipoAviones = {
-    marca: '',
-    modelo: '',
-    annio: 0,
-    cantPasajeros: 0,
-    cantFilas: 0,
-    cantAsientosFila: 0,
-  }
-
-  ngOnInit(): void {
-  }
-
-  tipoAvionForm = new FormGroup({
-    // definición de los campos del formulario, el valor inicial ('') y el tipo de validación (requerido)
     marca: new FormControl('', Validators.required),
     modelo: new FormControl('', Validators.required),
     annio: new FormControl('', Validators.required),
     cantPasajeros: new FormControl('', Validators.required),
     cantFilas: new FormControl('', Validators.required),
-    cantAsientosFila: new FormControl('', Validators.required),
+    cantAsientosPorFila: new FormControl('', Validators.required),
   });
+  
+  constructor(
+    private tipoAvionesService: TipoAvionesService,
+    private router: Router,
+    private activeRoute: ActivatedRoute
+  ) {}
 
-  submitForm() {
-    //validamos el formulario primeramente
-    if (this.tipoAvionForm.valid) {
-      // llamamos al servicio pasando el id del post y el valor del formulario que contien
-      // los campos que definimos anteriormente
-      this.tipoAvionesService
-        .addTipoAvion(this.tipoAviones.marca, this.tipoAviones.modelo,this.tipoAviones.annio, true ,this.tipoAviones.cantPasajeros, this.tipoAviones.cantFilas, this.tipoAviones.cantAsientosFila)
-        .subscribe((data) => {
-          this.tipoAviones = data;
-          this.tipoAvionForm.reset();
+  editMode = false;
+  post: any = {};
+
+  ngOnInit(): void {
+    this.activeRoute.params.subscribe((params) => {
+      if (params['id']) {
+        this.editMode = true;
+///para hacer el de actualizar/modificar
+        this.tipoAvionesService.getById(params['id']).subscribe((data) => {
+          this.post = data;
+          this.postForm.setValue({
+            nombre: data.nombre,
+            correo: data.correo,
+            usuario: data.usuario,
+            contrasennia: data.contrasennia,
+            apellidos: data.apellidos,
+            fechaNacimiento: data.fechaNacimiento,
+            telefonoCelular: data.telefonoCelular,
+            tipoUsuario: data.tipoUsuario,
+
+          });
         });
-    }
+      }
+    });
+    
+    
+  }
+ 
+
+  navigateToList() {
+    this.router.navigate(['/dashboard/blog/list']);
   }
 
+  submitForm() {
+    console.log("paso por aca 1");
+    if (this.postForm.valid) {
+      console.log("paso por aca2 ");
+      if (this.editMode) {
+        console.log("paso por aca 3");
+        this.tipoAvionesService
+          .edit(this.post._id, this.postForm.value)
+          .subscribe((data) => {
+            this.navigateToList();
+          });
+      } else {
+        console.log("paso por aca 4");
+        this.tipoAvionesService.create(this.postForm.value).subscribe((data) => {
+          this.navigateToList();
+        });
+      }
+    }
+  }
 }
+
