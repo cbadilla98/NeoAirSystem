ae7edc7851ba586fe8b84370b7e4510c0c5aa055
Cesar Badilla <79708649+cbadilla98@users.noreply.github.com>
2021-11-26 00:24:41 -0600

    Funcionalidad del mapa terminada

--- client/platform/src/app/app.module.ts
+++ client/platform/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AdminComponent } from './modulos/admin/admin/admin.component';
 import { UsuarioFormComponent } from './modulos/admin/usuario-form/usuario-form.component';
 import { MantenimientosComponent } from './layouts/mantenimientos/mantenimientos.component';
 import { UserComponent } from './layouts/user/user.component';
+import { GoogleMapsModule } from '@angular/google-maps'
 
 // import { authInterceptorProviders } from './helpers/auth.interceptor';
 // import { PostComponent } from './components/blog/post/post.component';
@@ -27,6 +28,7 @@ import { TipoAvionFormComponent } from './modulos/admin/tipo-avion-form/tipo-avi
     UsuarioFormComponent,
     TipoAvionComponent,
     TipoAvionFormComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -35,6 +37,8 @@ import { TipoAvionFormComponent } from './modulos/admin/tipo-avion-form/tipo-avi
     HttpClientModule,
     ReactiveFormsModule,
     NgbModule,
+    GoogleMapsModule
+    //AIzaSyAtvh2PaRvmpYDyOinNGFpDsOMEdWl4ESY
   ],
   providers: [],
   bootstrap: [AppComponent]
